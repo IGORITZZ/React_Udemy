34cf022d1170ec75f907ba7411a67e0cdd589538
Igor Theodoro <IGORITZZ@users.noreply.github.com>
2024-05-21 12:08:32 -0300

    att aula key

--- 3_AVANÇANDO_EM_REACT/avancando/src/components/LisntsRenders.jsx
+++ 3_AVANÇANDO_EM_REACT/avancando/src/components/LisntsRenders.jsx
@@ -2,12 +2,25 @@ import { useState } from 'react'
 
 const LisntsRenders = () => {
     const [list] = useState(['Igor', 'Bruno', 'Thomaz'])
+
+    const [users] = useState([
+      {id: 1, name:"Igor", age: 38},
+      {id: 68978978, name: 'Bruno', age: 28},
+      {id: 54884, name: 'Flávio', age: 18},
+      {id: 88896484, name: 'Teros', age: 8}
+    ])
+
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
             ) )}
+        </ul>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name} - {user.age}</li> 
+          ))}
         </ul>
     </div>
   )
