f10608ed159f9d3071400acf8bf2fb5f226a1231
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-01-22 17:02:15 -0300

    att

--- 1-Introdução/aula09-hello word/hello-word/src/App.jsx
+++ 1-Introdução/aula09-hello word/hello-word/src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
     <h1>Hello Word</h1>
+    <h2>Emmet Funcionou</h2>
+    
     </>
   )
 }
---
+++ 2 - Fundamentos/aula-23/criando-componentes/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import FirstComponent from './compontes/FirstComponent'
+
+function App() {
+  return (
+    <div className="name">
+      <h1>Importando meu Primeiro componente</h1>
+      <p>Tag errada</p>
+      <FirstComponent/>
+    </div>
+     
+
+  )
+}
+
+export default App
