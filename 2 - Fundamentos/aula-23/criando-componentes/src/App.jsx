7a4b9b4773bcd39389c880747d21601e4f6fc47f
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-01-23 17:15:00 -0300

    Update App.jsx

--- 2 - Fundamentos/aula-23/criando-componentes/src/App.jsx
+++ 2 - Fundamentos/aula-23/criando-componentes/src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
 import FirstComponent from './compontes/FirstComponent'
+import MyComponent from './compontes/MyComponent'
+import MyEvent from './compontes/MyEvent'
+import TemplateExpression from './compontes/TemplateExpression'
 
+//comentário
+/*
+  comentário
+*/ 
 function App() {
   return (
+    //comentário
     <div className="name">
+      {/* comentário */}
       <h1>Importando meu Primeiro componente</h1>
       <p>Tag errada</p>
       <FirstComponent/>
+      <TemplateExpression/>
+      <MyComponent/>
+      <MyEvent/>
     </div>
      
 
