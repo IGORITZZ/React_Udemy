edade3c0374a41043ad9560dcab87fac482feb27
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-01-24 12:06:39 -0300

    Update MyEvent.jsx

--- 2 - Fundamentos/aula-23/criando-componentes/src/compontes/MyEvent.jsx
+++ 2 - Fundamentos/aula-23/criando-componentes/src/compontes/MyEvent.jsx
@@ -1,15 +1,41 @@
 const MyEvent = () => {
-    const handleMyEvent = (evento) => {
-        console.log(evento)
-        console.log("Ativou o Evento")
-    } 
-    return(
-        <div>
-            <div>
-                <button onClick={handleMyEvent} >Clique Aqui</button>
-            </div>
-        </div>
-    )
-}
+  const handleMyEvent = (e) => {
+    console.log(e);
+    console.log("Ativou o Evento");
+  };
 
-export default MyEvent
+  const renderSomenthin = (x) => {
+    if (x) {
+      return <h1>RENDERIZANDO TRUE</h1>;
+    } else {
+      return <h1>REDENRIZANDO FALSE</h1>;
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={handleMyEvent}>Clique Aqui</button>
+      </div>
+      <div>
+        <button onClick={() => console.log("Clicou também")}>
+          Clique Aqui Também
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            if (true) {
+              console.log("Não deveria cliciar, isso está errado");
+            }
+          }}
+        >
+          Clique Aqui Também, por favor!!!
+        </button>
+      </div>
+      {renderSomenthin(true)}
+      {renderSomenthin(false)}
+    </div>
+  );
+};
+
+export default MyEvent;
