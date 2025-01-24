680fa454d8242e82bf23992b3392d520962803a6
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-01-24 13:00:17 -0300

    nova seção

---
+++ 3 - Avançando em React/aula-35/imagens-react/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Avaçando em React</h1>
+      {/* imagem public */}
+      <div>
+        <img src="/img1.jpg" alt="" />
+      </div>
+    </>
+  )
+}
+
+export default App
