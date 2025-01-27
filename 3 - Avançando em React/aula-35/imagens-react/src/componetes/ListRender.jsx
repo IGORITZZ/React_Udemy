d5d99ddf25df644ae770121fa3fac53d71c233c1
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-01-27 20:02:47 -0300

    Update ListRender.jsx

--- 3 - Avançando em React/aula-35/imagens-react/src/componetes/ListRender.jsx
+++ 3 - Avançando em React/aula-35/imagens-react/src/componetes/ListRender.jsx
@@ -3,11 +3,24 @@ import { useState } from "react";
 const ListRender = () => {
   const [list] = useState(["Igor", "Laura", "Larissa", "Miguel"]);
 
+  const [users] = useState([
+    { id: 1, name: "José", age: 27 },
+    { id: 461968498, name: "Mauro", age: 23 },
+    { id: 87417849849, name: "Trevis", age: 17 },
+    { id: 423423424222, name: "Mouses", age: 18 },
+  ]);
   return (
     <div>
       <ul>
-        {list.map((item) => (
-          <li>{item}</li>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age}
+          </li>
         ))}
       </ul>
     </div>
