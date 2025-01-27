d3a429548d34078a7fe9a9f01926836ec42043d5
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-01-27 16:30:56 -0300

    Update App.jsx

--- 3 - Avançando em React/aula-35/imagens-react/src/App.jsx
+++ 3 - Avançando em React/aula-35/imagens-react/src/App.jsx
@@ -1,5 +1,8 @@
 import './App.css'
 
+import city from "./assets/city.jpg"
+import ManageData from './componetes/ManageData'
+
 function App() {
 
   return (
@@ -7,8 +10,13 @@ function App() {
       <h1>Avaçando em React</h1>
       {/* imagem public */}
       <div>
-        <img src="/img1.jpg" alt="" />
+        <img src="/img1.jpg" alt="paisagem" />
       </div>
+      {/* imagem em src */}
+      <div>
+      <img src={city} alt="cidade" />
+      </div>
+      <ManageData/>
     </>
   )
 }
