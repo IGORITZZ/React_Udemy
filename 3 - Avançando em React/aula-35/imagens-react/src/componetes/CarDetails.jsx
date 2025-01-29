3fed49ecaf9383b41462a43a6691f6a8168fae43
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-01-29 16:54:46 -0300

    Update CarDetails.jsx

--- 3 - Avançando em React/aula-35/imagens-react/src/componetes/CarDetails.jsx
+++ 3 - Avançando em React/aula-35/imagens-react/src/componetes/CarDetails.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-
-const CarDetails = ({marca, km, cor}) => {
+const CarDetails = ({marca, km, cor, carroNovo}) => {
   return (
     <>
-    <h1>Detalhes do carro</h1>
-    <ul>
+      <h1>Detalhes do carro</h1>
+      <ul>
         <li>Marca: {marca}</li>
         <li>KM: {km}</li>
         <li>Cor: {cor}</li>
-    </ul>
+        {carroNovo && <p>Esse carro é zero</p>}
+        {!carroNovo && <p>Esse carro é usado</p>}
+      </ul>
     </>
-  )
+  );
 }
 
 export default CarDetails
