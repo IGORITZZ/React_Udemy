ee90ff6c1ec0a5effa53c7b6cd1310897c593d7b
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-02-06 15:32:02 -0300

    Create ExecuteFuinction.jsx

---
+++ 3 - Avançando em React/aula-35/imagens-react/src/componetes/ExecuteFuinction.jsx
@@ -0,0 +1,10 @@
+
+const ExecuteFuinction = ({minhaFuncao}) => {
+  return (
+    <div>
+        <button onClick={minhaFuncao}>Clique Aqui para executar</button>
+    </div>
+  )
+}
+
+export default ExecuteFuinction
