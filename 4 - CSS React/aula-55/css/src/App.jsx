824428e3c65f242459b3841a3c23b0d984475fed
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-02-20 12:18:28 -0300

    seção 4

---
+++ 4 - CSS React/aula-55/css/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import MeuComponete from './componentes/MeuComponete'
+
+function App() {
+  return (
+    <>
+       {/*  CSS Global */}
+      <h1>React com CSS</h1>
+      <p>Paragrafo do JSX</p>
+      {/* CSS de componente */}
+      <MeuComponete />
+    </>
+  )
+}
+
+export default App
---
+++ 4 - CSS React/aula-55/css/src/componentes/MeuComponete.jsx
@@ -0,0 +1,11 @@
+import './MeuComponete.css'
+
+const MeuComponete = () => {
+  return <>
+  <h1>Titulo do Meu Componete</h1>
+  <p>Paragrafo do componente</p>
+  <p className="p-meu-componente">Aqui também pertence ao componente</p>
+  </>;
+};
+
+export default MeuComponete;
