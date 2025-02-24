a6a330ad07d48972fd86ab74c8e523def9bfd299
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-02-24 16:07:21 -0300

    aula 58

--- 4 - CSS React/aula-55/css/src/App.jsx
+++ 4 - CSS React/aula-55/css/src/App.jsx
@@ -1,16 +1,26 @@
-import './App.css'
-import MeuComponete from './componentes/MeuComponete'
+import "./App.css";
+import MeuComponete from "./componentes/MeuComponete";
 
 function App() {
   return (
     <>
-       {/*  CSS Global */}
+      {/*  CSS Global */}
       <h1>React com CSS</h1>
       <p>Paragrafo do JSX</p>
       {/* CSS de componente */}
       <MeuComponete />
+      {/* Inline CSS */}
+      <p
+        style={{
+          color: "white",
+          fontWeight: "900",
+          borderTop: "2px solid red",
+        }}
+      >
+        Esse paragrafo é com CSS Inline
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
