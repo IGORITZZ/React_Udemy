1c9665b9c08e1d902ec0803ce340a5e2d72dff35
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-03-24 10:44:59 -0300

    att

--- 4 - CSS React/Desafio 5/challengecss/src/componentes/ListaCarros.jsx
+++ 4 - CSS React/Desafio 5/challengecss/src/componentes/ListaCarros.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import "./ListaCarros.componente.css";
+import styles from "./ListaCarros.module.css";
 
 
 const ListaCarros = ({marca, modelo, km, cor}) => {
   return (
-    <div className='lista_carros'>
+    <div className={styles.lista_carros}>
       <p>{marca}</p>
       <p>{modelo}</p>
       <p>{km}</p>
