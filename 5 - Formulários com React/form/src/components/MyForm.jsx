d7f40a0891a9b9170b0c0678b503dedc75c9aa5f
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-03-25 21:58:50 -0300

    Update MyForm.jsx

--- 5 - Formulários com React/form/src/components/MyForm.jsx
+++ 5 - Formulários com React/form/src/components/MyForm.jsx
@@ -1,16 +1,34 @@
+import { useState } from 'react'
 import './MyForm.css'
 
 const MyForm = () => {
+  {/* 3. Gerenciamento de Dados */}
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
+  console.log(name)
+
   return (
     <div>
-        {/* 1. Criando um Formulário*/}
-        <form>
-            <label htmlFor="nome">Nome: </label>
-            <input type="nome" name='nome' placeholder='Digite Seu nome'/>
-            <input type="buttom" value='Enviar' />
-        </form>
+      {/* 1. Criando um Formulário*/}
+      <form>
+        <div>
+          <label htmlFor="nome">Nome: </label>
+          <input type="nome" name="nome" placeholder="Digite Seu nome" onChange={handleName}/>
+          {/* 2 - Label envolvendo input */}
+        </div>
+        <label>
+          <span>E-mail</span>
+          <input type="email" name="email" placeholder="Digite o seu e-mail" />
+        </label>
+        <input type="button" value="Enviar" />
+      </form>
     </div>
-  )
+  );
 }
 
 export default MyForm
