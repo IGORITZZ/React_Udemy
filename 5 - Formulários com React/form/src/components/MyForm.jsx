94b953ac136670df71f194a5133012ec23442217
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-03-27 15:00:45 -0300

    Update MyForm.jsx

--- 5 - Formulários com React/form/src/components/MyForm.jsx
+++ 5 - Formulários com React/form/src/components/MyForm.jsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import './MyForm.css'
+import { useState } from "react";
+import "./MyForm.css";
 
 const MyForm = () => {
-  {/* 3. Gerenciamento de Dados */}
+  {
+    /* 3. Gerenciamento de Dados */
+  }
   const [name, setName] = useState();
   const [email, setEmail] = useState();
 
   const handleName = (e) => {
-    setName(e.target.value)
-  }
-
-  console.log(name)
+    setName(e.target.value);
+  };
+  console.log(name);
+  console.log(email);
 
   return (
     <div>
@@ -18,17 +20,27 @@ const MyForm = () => {
       <form>
         <div>
           <label htmlFor="nome">Nome: </label>
-          <input type="nome" name="nome" placeholder="Digite Seu nome" onChange={handleName}/>
-          {/* 2 - Label envolvendo input */}
+          <input
+            type="nome"
+            name="nome"
+            placeholder="Digite seu nome"
+            onChange={handleName} // aula 3
+          />
         </div>
+        {/* 2 - Label envolvendo input */}
         <label>
           <span>E-mail</span>
-          <input type="email" name="email" placeholder="Digite o seu e-mail" />
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o seu e-mail"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <input type="button" value="Enviar" />
       </form>
     </div>
   );
-}
+};
 
-export default MyForm
+export default MyForm;
