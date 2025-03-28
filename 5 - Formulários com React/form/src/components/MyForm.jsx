ec04e14d61a3539ee693d2b53e36e7716bfc0660
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-03-28 15:22:14 -0300

    Update MyForm.jsx

--- 5 - Formulários com React/form/src/components/MyForm.jsx
+++ 5 - Formulários com React/form/src/components/MyForm.jsx
@@ -1,23 +1,40 @@
 import { useState } from "react";
 import "./MyForm.css";
 
-const MyForm = () => {
-  {
-    /* 3. Gerenciamento de Dados */
-  }
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-
+const MyForm = ({ user }) => {
+  //6. Controlled inputs
+  //3. Gerenciamento de Dados
+  const [name, setName] = useState(user ? user.name : "");
+  const [email, setEmail] = useState(user ? user.email : "");
+  const [bio, setBio] = useState("");
+  
   const handleName = (e) => {
     setName(e.target.value);
   };
-  console.log(name);
-  console.log(email);
+  //console.log(name);
+  //console.log(email);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Enviando Formulário...");
+    console.log("Formulário enviado com sucesso!");
+    console.log("Nome: " + name);
+    console.log("Email: " + email);
+    console.log("Bio: " + bio);
+
+    //7. Limpar Formulário
+    setName("");
+    setEmail("");
+    setBio("");
+  };
+
+
 
   return (
     <div>
+      {/* //5 - envio de FORM */}
       {/* 1. Criando um Formulário*/}
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="nome">Nome: </label>
           <input
@@ -25,19 +42,32 @@ const MyForm = () => {
             name="nome"
             placeholder="Digite seu nome"
             onChange={handleName} // aula 3
+            value={name}
           />
         </div>
         {/* 2 - Label envolvendo input */}
         <label>
+          {/* 4 - simplificação de maninupulação */}
           <span>E-mail</span>
           <input
             type="email"
             name="email"
             placeholder="Digite o seu e-mail"
             onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
         </label>
-        <input type="button" value="Enviar" />
+        {/* 8. Textarea no React */}
+        <label>
+          <span>Bio:</span>
+          <textarea
+            name="bio"
+            placeholder="Descrição..."
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+          ></textarea>
+        </label>
+        <input type="submit" value="Enviar" />
       </form>
     </div>
   );
