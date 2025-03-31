a1f9247f3c25f4dfc73dba71f58adc0f6e17eeb8
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-03-31 15:30:45 -0300

    projeto secret-word

---
+++ 6 - Projeto Secret Word/secret-word/src/data/words.jsx
@@ -0,0 +1,13 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", ],
+  marca: ["Fiat", "Renault", "Citroën", "Hyundai", "Chevrolet", "Volkswagen", "Toyota"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Abacate", "Abacaxi", "Açaí", 
+    "Acerola", "Amora", "Araticum", "Bacaba", "Banana", "Biribá", "Cacau"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
+
+
+
