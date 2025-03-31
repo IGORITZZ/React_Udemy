e4c965d558b5e4a7c8082815bf79db3e763d4fec
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-03-31 13:06:53 -0300

    Update App.jsx

--- 5 - Formulários com React/form/src/App.jsx
+++ 5 - Formulários com React/form/src/App.jsx
@@ -1,14 +1,20 @@
-import './App.css'
-import MyForm from './components/MyForm'
+import "./App.css";
+import MyForm from "./components/MyForm";
 
 function App() {
-
   return (
     <>
       <h2>Formulário</h2>
-      <MyForm user={{name: 'Igor', email: 'igor@teste.com'}}/> 
+      <MyForm
+        user={{
+          name: "Igor",
+          email: "igor@teste.com",
+          bio: "Eu sou um Adm do sistema",
+          role: "admin",
+        }}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
