c09ea0f3b6154e5450ac72eaa0aead68764d6ddd
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-04-01 16:41:13 -0300

    Create StarScreen.jsx

---
+++ 6 - Projeto Secret Word/secret-word/src/components/StarScreen.jsx
@@ -0,0 +1,13 @@
+import './StarScreen.css'
+
+const StarScreen = () => {
+  return (
+    <div className='start'>
+        <h1>Palavra Secreta</h1>
+        <p>Clique no botão abaixo para começar</p>
+        <button>Comerçar o jogo</button>
+    </div>
+  )
+}
+
+export default StarScreen
