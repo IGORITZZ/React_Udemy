d4cea37b6e63d3f4a4be726f4596258c8c781c43
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-04-02 17:36:59 -0300

    Update App.jsx

--- 6 - Projeto Secret Word/secret-word/src/App.jsx
+++ 6 - Projeto Secret Word/secret-word/src/App.jsx
@@ -1,12 +1,28 @@
+// CSS
 import './App.css'
+// React
+import { useCallback, useEffect, useState } from 'react'
+// Data
+import {wordsList} from './data/words'
+console.log(wordsList);
+// Componentes
 import StarScreen from './components/StarScreen'
 
+//Estágios do jogo
+const stages = [
+  {id: 0, name: 'start'},
+  {id: 1, name: 'game'},
+  {id: 2, name: 'gameover'}
+]
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
 
   return (
     <>
       <div className='App'>
-        <StarScreen/>
+        {gameStage === 'start' &&  <StarScreen/>}
+        {gameStage === 'game' && <Game/>}
+        {gameStage === 'gameover' && <GameOver/>}
       </div>
     </>
   )
