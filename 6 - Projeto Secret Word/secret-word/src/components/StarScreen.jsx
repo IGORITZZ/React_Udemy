f3f606c8ff97cc717d0fcbb50509bd486a631bf5
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-04-03 15:14:18 -0300

    Update StarScreen.jsx

--- 6 - Projeto Secret Word/secret-word/src/components/StarScreen.jsx
+++ 6 - Projeto Secret Word/secret-word/src/components/StarScreen.jsx
@@ -1,11 +1,11 @@
 import './StarScreen.css'
 
-const StarScreen = () => {
+const StarScreen = ({startGame}) => {
   return (
     <div className='start'>
         <h1>Palavra Secreta</h1>
         <p>Clique no botão abaixo para começar</p>
-        <button>Comerçar o jogo</button>
+        <button onClick={startGame}>Comerçar o jogo</button>
     </div>
   )
 }
