3b01827859c6dad9785d7a9ed67d6f75aed49d52
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-04-07 21:33:53 -0300

    Update App.jsx

--- 6 - Projeto Secret Word/secret-word/src/App.jsx
+++ 6 - Projeto Secret Word/secret-word/src/App.jsx
@@ -11,9 +11,9 @@ import Game from './components/Game'
 import GameOver from './components/GameOver'
 //Estágios do jogo
 const stages = [
-  {id: 1, name: 'start'},
-  {id: 2, name: 'game'},
-  {id: 3, name: 'gameover'}
+  {id: 0, name: 'start'},
+  {id: 1, name: 'game'},
+  {id: 2, name: 'gameover'}
 ]
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
