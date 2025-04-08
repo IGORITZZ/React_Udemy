30a5b2934145f2e0f4d1a67c996f6bce69799e09
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-04-08 16:16:15 -0300

    Update Game.jsx

--- 6 - Projeto Secret Word/secret-word/src/components/Game.jsx
+++ 6 - Projeto Secret Word/secret-word/src/components/Game.jsx
@@ -2,11 +2,32 @@ import './Game.css'
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar Jogo</button>
+    <div className="game">
+      <p className="pontos">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Advinhe a Palavra:</h1>
+      <h3 className='dica'>
+        Dica sobre a palavra: <span>dica...</span>
+      </h3>
+      <div className="palavraContainer">
+        <span className='letra'>a</span>
+        <span className="quadroBranco"></span>
+      </div>
+      <div className="letraContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength='1'required/>
+          <button>Jogar</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a, </span>
+        <span>b, </span>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Game
