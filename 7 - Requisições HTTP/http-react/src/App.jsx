ea4a40063ff55f57e52109098ec9c19d701bc99a
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-04-30 12:07:17 -0300

    Update App.jsx

--- 7 - Requisições HTTP/http-react/src/App.jsx
+++ 7 - Requisições HTTP/http-react/src/App.jsx
@@ -1,11 +1,32 @@
 import './App.css'
 
+import { useState, useEffect } from 'react'
+
+const url = "http://localhost:3000/produtos";
+
 function App() {
+
+  const [produtos, setProdutos] = useState([])
+
+  // 1 - Resgatando dados
+  useEffect(() => {
+    async function fecthData() {
+      const resposta = await fetch(url);
+      const data = await resposta.json();
+      setProdutos(data);
+    }
+    fecthData()
+  }, [])
 
   return (
     <>
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {produtos.map((produto) => (
+          <li key={produto.id}>{produto.name} R$: {produto.preco}</li>
+        ))}
+      </ul>
     </div>
     </>
   )
