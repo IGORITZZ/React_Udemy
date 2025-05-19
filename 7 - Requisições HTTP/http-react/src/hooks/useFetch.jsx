39056e13d2a7c7e003567f9d4a92ad86994b48b1
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-05-19 14:55:25 -0300

    Update useFetch.jsx

--- 7 - Requisições HTTP/http-react/src/hooks/useFetch.jsx
+++ 7 - Requisições HTTP/http-react/src/hooks/useFetch.jsx
@@ -1,19 +1,52 @@
 import { useState, useEffect } from "react";
 
-    // 1 e 4 - customizando hook e resgatando dados
-  export const useFetch = (url) => {
-    const [dados, setDados] = useState(null)
+// 1 e 4 - customizando hook e resgatando dados
+export const useFetch = (url) => {
+  const [dados, setDados] = useState(null);
+  // 5 - refatorando o POST
+  const [configuracao, setConfiguracao] = useState(null);
+  const [metodo, setMetodo] = useState(null);
+  const [recarregamento, setRecarregamento] = useState(false);
 
-    useEffect(() => {
+  const configuracaoHttp = (data, method) => {
 
-        const buscarDados = async () => {
-            const resposta = await fetch(url)
-            const dados = await resposta.json()
-            setDados(dados) 
-        }
-        buscarDados()
+    if(method === "POST"){
+        setConfiguracao({
+            method,
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        setMetodo(method)
+    }
 
-    }, [url])
+  }
 
-    return{dados}
-}
+  useEffect(() => {
+    const buscarDados = async () => {
+      const resposta = await fetch(url);
+      const dados = await resposta.json();\gi
+      setDados(dados);
+    };
+    buscarDados();
+  }, [url, recarregamento]);
+
+  // 5 - refatorando o POST
+  useEffect(() => {
+    const requisicaoHttp = async () => {
+      if (metodo === "POST") {
+        let buscarOpcoes = [url, configuracao];
+
+        const resposta = await fetch(...buscarOpcoes);
+
+        const json = await resposta.json();
+
+        setRecarregamento(json);
+      }
+    };
+    requisicaoHttp();
+  }, [configuracao, metodo, url]);
+
+  return { dados, configuracaoHttp };
+};
