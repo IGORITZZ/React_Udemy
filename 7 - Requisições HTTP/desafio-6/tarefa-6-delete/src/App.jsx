1d455663ace70cc4e337fe0767f871b9afc5330a
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-06-18 13:54:18 -0300

    att

--- 7 - Requisições HTTP/desafio-6/tarefa-6-delete/src/App.jsx
+++ 7 - Requisições HTTP/desafio-6/tarefa-6-delete/src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const[data, useData] = useState(null)
+  
+
   return (
     <div>
-      <div className="quadrado"><p>Hello Word</p></div>
+      <div className="quadrado"><p>Lista de Produtos</p></div>
     </div>
   )
 }
---
+++ 7 - Requisições HTTP/desafio-6/tarefa-6-delete/src/assets/useFecth.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const useFecth = () => {
+  return (
+
+    <div>
+
+    </div>
+  )
+}
+
+export default useFecth
