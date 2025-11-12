d31a13d651970dbcd8bbcecbb1d7f5d64c65d802
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-11-11 23:48:55 -0300

    Update Produto.jsx
    fim da aula sobre rotas dinamicas

--- 8 - React Router/react-router-dom/src/pages/Produto.jsx
+++ 8 - React Router/react-router-dom/src/pages/Produto.jsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom"
+
+
 const Produto = () => {
-  return (
-    <div>Produto</div>
-  )
+  // 4 - Rotas dinamicas
+  const { id } = useParams();
+  return <div>
+    <p>ID do produto: {id}</p>
+  </div>;
 }
 
 export default Produto
