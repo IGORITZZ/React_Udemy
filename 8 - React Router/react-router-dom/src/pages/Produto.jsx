d9d9e4237605dd2dbee4db9218fee0462a8dca6a
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-11-12 09:03:21 -0300

    Update Produto.jsx
    fazer o carregamento do produto de forma individual utilizando carregamento dinamico

--- 8 - React Router/react-router-dom/src/pages/Produto.jsx
+++ 8 - React Router/react-router-dom/src/pages/Produto.jsx
@@ -1,12 +1,26 @@
-import { useParams } from "react-router-dom"
-
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
 
 const Produto = () => {
   // 4 - Rotas dinamicas
   const { id } = useParams();
-  return <div>
-    <p>ID do produto: {id}</p>
-  </div>;
-}
 
-export default Produto
+  // 5 -  Carregamento de dado individual
+  const url = "http://localhost:3000/produtos/" + id;
+  const { data: produto, loading, error } = useFetch(url);
+  return (
+    <div>
+      <p>ID do produto: {id}</p>
+      {error && <p>Houve um erro no carregamento...</p>}
+      {loading && <p>Carregando produto...</p>}
+      {produto && (
+        <div>
+          <h1>{produto.nome}</h1> 
+          <p>R$ {produto.valor}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Produto;
