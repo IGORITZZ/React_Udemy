dc16d8c5a2c7b016fdd3db3bb34e615555ac0c9b
Igor Theodoro <143856268+IGORITZZ@users.noreply.github.com>
2025-12-11 09:13:45 -0300

    att

--- 9 - Context API do React/context-api/src/context/CounterContext.jsx
+++ 9 - Context API do React/context-api/src/context/CounterContext.jsx
@@ -1,14 +1,13 @@
-//1- criar contexto
 import { createContext, useState } from "react";
 
 export const CounterContext = createContext();
 
-//2- criar provider
-export const CounterContexProvider =({children}) => {
-    const [counter, setConter] = useState(5)
-    return(
-        <CounterContext.Provider value={{counter, setConter}}>
-            {children}
-        </CounterContext.Provider>
-    )
-}
+export const CounterContextProvider = ({ children }) => {
+  const [counter, setCounter] = useState(5);
+
+  return (
+    <CounterContext.Provider value={{ counter, setCounter }}>
+      {children}
+    </CounterContext.Provider>
+  );
+};
--- 9 - Context API do React/context-api/src/main.jsx
+++ 9 - Context API do React/context-api/src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { CounterContexProvider } from './context/CounterContext.jsx'
+import { CounterContextProvider } from './context/CounterContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CounterContexProvider>
+    <CounterContextProvider>
       <App />
-    </CounterContexProvider>
+    </CounterContextProvider>
   </StrictMode>,
 )
